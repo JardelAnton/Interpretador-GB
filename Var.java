a108072ff566b1709e04aebae81d95ab852da343
JoaoBarp <joaorbarp@hotmail.com>
2015-04-22 09:50:40 -0300

    var

--- Var.java
+++ Var.java
@@ -2,9 +2,13 @@ class Var{
 	private double valor;
 	public String nome;
 
+	public Var (String j,Double l){
+		this.valor=l;
 		this.nome=j;
 
 	}
+	public void setValor (double l) {
+		this.valor=l;
 	}
 	public double getValor (){
 		return this.valor;
