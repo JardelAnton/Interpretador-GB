cc7e2d82013f210d96e9984925376e8698443542
JoaoBarp <joaorbarp@hotmail.com>
2015-04-22 11:27:47 -0300

    imprime

--- Interpretador.java
+++ Interpretador.java
@@ -16,6 +16,9 @@ class Interpretador {
 
         for(int i = 0; i < this.linhas.length; i++) {
             if(this.linhas[i] != NULL) {
+            	if(this.linhas[i].contains("#Imprime")==1){
+            		this.imprime(this.linhas[i]);
+            	}
                 // TODO: interpretar a linha
                 System.out.println("Linha " + (i + 1) + ": " + this.linhas[i]);
             }
@@ -24,7 +27,30 @@ class Interpretador {
 
 
     public void imprime (String h) {
+    	int i;
+    	double var;
+    	int c;
+    	int aux;
+    	String [5000]j;
+    	for(i=0;i<h.length;i++){
+    		aux=i;
+    		if(h[i]=='('){
+    			break;
+    		}
+    	}
+    	for(i=0;h[aux] != ')';i++){
+    		c++;
+    		j[i]=h[aux];
+    		aux++;
+    	}
+    	for(i=0;i<c;i++){
+    		if(h[i] != '$'){
+    			System.out.println(this.j[i]);
+    		}else{
+    			//verifica variavel.
+    		}
 
+    	}
     }
 
 }
