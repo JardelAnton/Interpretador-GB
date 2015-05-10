14bb20d0c0fa7840c5cbf263b0b914d7ab993335
JoaoBarp <joaorbarp@hotmail.com>
2015-05-10 16:24:51 -0300

    If correto

--- Interpretador.java
+++ Interpretador.java
@@ -5,10 +5,11 @@
 class Interpretador {
     private String linhas[];
     private Matematica mat= new Matematica();
-    private Boolean valida = true;
+    private boolean[] valida = new boolean[200];
+    private int escopoIf=0;
 
     public void interpreta(String l[]) {
-
+        valida[escopoIf]=true;
         this.linhas = l;
         //retira os espaços, menos em linhas que tem imprime
         for(int i=0; linhas[i] != null; i++){
@@ -20,31 +21,34 @@ class Interpretador {
         for(int i = 0; i < this.linhas.length; i++) {
             if(this.linhas[i] != null) {
             	if(this.linhas[i].contains("#Imprime")==true){
-                    if(valida==true){
+                    if(valida[escopoIf]==true){
             		  mat.imprime(this.linhas[i]);
                     }
             	}else if(this.linhas[i].contains("#Var")==true){
-                    if(valida==true){
+                    if(valida[escopoIf]==true){
                         mat.criaVar(this.linhas[i]);
                     }
                 }else if(this.linhas[i].contains("#Se")==true){
-                    if(valida==true){
-                        valida=mat.leExp(this.linhas[i]);
+                    if(valida[escopoIf]==true){
+                        escopoIf++;
+                        valida[escopoIf]=mat.leExp(this.linhas[i]);
+                    }else{
+                        escopoIf++;
                     }
                 }else if(this.linhas[i].contains("#Le")==true){
-                    if(valida==true){
+                    if(valida[escopoIf]==true){
                         mat.scan(this.linhas[i]);
                     }
                 }else if(this.linhas[i].contains("#Enquanto")==true){
-                     if(valida==true){
+                     if(valida[escopoIf]==true){
                      //mat.criaVar(this.linhas[i])
                      }
                 }else if(this.linhas[i].contains("#Fimse")==true){
-                    valida=true;
+                    escopoIf--;
                 }else if(this.linhas[i].contains("#Fimenquanto")==true){
                     //mat.criaVar(this.linhas[i])
                 }else if(this.linhas[i].contains("=")==true){
-                    if(valida==true){
+                    if(valida[escopoIf]==true){
                         mat.atribuicao(this.linhas[i]);
                     }
                 }else{
