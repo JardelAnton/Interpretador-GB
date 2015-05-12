89bc5663b89bbafe79716ddc4478401f1b8d8439
JoaoBarp <joaorbarp@hotmail.com>
2015-05-12 11:43:46 -0300

    Classe matematica

--- Matematica.java
+++ Matematica.java
@@ -2,12 +2,7 @@
 
 
 
-
-
-
-
 import java.util.Scanner;
-
 class Matematica {
 	Var []variaveis;
 	private int topo;
@@ -20,7 +15,6 @@ class Matematica {
         }
 		topo=0;
 	}
-	
 	public void criaVar(String h){
 		int aux=0;
 		String var;
@@ -38,6 +32,8 @@ class Matematica {
 		//}
 	}
 
+	
+
 
 	public void atribuicao (String h) {
 		String var_1,var_2,var_3;
@@ -48,11 +44,7 @@ class Matematica {
 		var_1=this.retVar('=',h,0);
 		aux=this.achar(h,'=');
     	StringBuilder sb = new StringBuilder();
-
-		for(int i=aux;h.charAt(i) != '+' && h.charAt(i) != '-' && h.charAt(i) != '*' && h.charAt(i) != '/' && h.charAt(i) != ';' && h.charAt(i) != '%';i++){
-
 		for(int i=aux;h.charAt(i) != '+' && h.charAt(i) != '-' && h.charAt(i) != '*' && h.charAt(i) != '/' && h.charAt(i) != ';';i++){
-
 			sb.append(h.charAt(i));
 			aux=i;
 		}
@@ -70,13 +62,7 @@ class Matematica {
 		if(pqp == 2){
 			a=this.pegaValor(var_2);
 			b=this.pegaValor(var_3);
-
-
-		if(pqp == 2){
-			a=this.pegaValor(var_2);
-			b=this.pegaValor(var_3);
 	
-
 		}else{
 			a=this.pegaValor(var_2);
 		}
@@ -109,9 +95,7 @@ class Matematica {
 			}else{
 				a=Double.parseDouble(h);
 			}
-
 		
-
 			return a;
 	}
 	
@@ -121,6 +105,7 @@ class Matematica {
 				return true;
 			}
 		return false;
+
 	}
 
 	public double operacao(double a, double b, char c){
@@ -136,12 +121,6 @@ class Matematica {
 		if(c == '/'){
 			return (a/b);
 		}
-
-		if(c == '%'){
-			return (a%b);
-		}
-
-
 
 		return a;
 	}
@@ -167,6 +146,7 @@ class Matematica {
     	}
     	return aux;
 	}
+
 
 	public void imprime (String h) {
     	int i,j,g=1;
@@ -194,7 +174,8 @@ class Matematica {
     				System.out.println("Var n existe");
     			}
     		}
-	  	}
+
+    	}
     }
 
     public String retVar (char c,String h, int aux){
@@ -225,7 +206,6 @@ class Matematica {
 			variaveis[aux].setValor(valor);
 		}
     }
-
 
     public boolean leExp(String h){
     	boolean x;
@@ -335,105 +315,5 @@ class Matematica {
 	}
 
 
-    public boolean leExp(String h){
-    	boolean x;
-    	String var_1,var_2,var_3;
-		String boo;
-		char tok;
-		int aux,con=0;
-		double a,b,c;
-		aux=this.achar(h,'(');
-		StringBuilder sb = new StringBuilder();
-		for(int i=aux;h.charAt(i) != '=' && h.charAt(i) != '<' && h.charAt(i) != '>' && h.charAt(i) != '#';i++){
-			sb.append(h.charAt(i));
-			aux=i;
-		}
-		var_1 = sb.toString();
-		sb = new StringBuilder();
-		sb.append(h.charAt(aux+1));
-		sb.append(h.charAt(aux+2));
-		boo = sb.toString();
-		sb = new StringBuilder();
-		aux++;
-		for(int i=aux+2;h.charAt(i) != '+' && h.charAt(i) != '-' && h.charAt(i) != '*' && h.charAt(i) != '/'&& h.charAt(i) != ')';i++){
-			sb.append(h.charAt(i));
-			aux=i;
-		}
-		var_2 = sb.toString();
-		tok=h.charAt(aux+1);
-		aux++;
-		if (h.charAt(aux)==')') {
-			con=1;
-			var_3="";
-		}else{
-			var_3=this.retVar(')',h,aux+1);
-			con=2;
-		}
-		if (con==2) {
-			a=this.pegaValor(var_1);
-			b=this.pegaValor(var_2);
-			c=this.pegaValor(var_3);
-			b=this.operacao(b,c,tok);
-			x=this.ler(a,b,boo);
-		}else{
-			a=this.pegaValor(var_1);
-			b=this.pegaValor(var_2);
-			x=this.ler(a,b,boo);
-		}
-		/*
-		System.out.println(var_1);
-		System.out.println(boo);
-		System.out.println(var_2);
-		System.out.println(tok);
-		System.out.println(var_3);
-		System.out.println("");
-		System.out.println("");
-		*/
-		return x;
-    }
-	public boolean ler(double a, double b, String tok){
-		if(tok.equals("==")){
-			if(a==b){
-				return true;
-			}else{
-				return false;
-			}
-		}
-		if(tok.equals("#=")){
-			if(a!=b){
-				return true;
-			}else{
-				return false;
-			}
-		}
-		if(tok.equals(">>")){
-			if(a>b){
-				return true;
-			}else{
-				return false;
-			}
-		}
-		if(tok.equals("<<")){
-			if(a<b){
-				return true;
-			}else{
-				return false;
-			}
-		}
-		if(tok.equals("<=")){
-			if(a<=b){
-				return true;
-			}else{
-				return false;
-			}
-		}
-		if(tok.equals(">=")){
-			if(a>=b){
-				return true;
-			}else{
-				return false;
-			}
-		}
-		return false;
-	}
+
 }
